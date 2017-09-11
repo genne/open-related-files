f55df405f65c9bce11590b29cb8ce03775f8f03f
Christian Genne <christian@jointrine.com>
2017-09-11 08:34:05 +0200

    VSCode extension to open related files

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from "path";
+import * as fs from "fs";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,14 +13,36 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "open-related-files" is now active!');
 
+    context.subscriptions.push(vscode.commands.registerCommand("extension.findComponent", async () => {
+        let result = await vscode.commands.executeCommand("workbench.action.quickOpen");
+        await vscode.commands.executeCommand("extension.openRelatedFiles");
+    }));
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.openRelatedFiles', async () => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        var editor = vscode.window.activeTextEditor;
+        let currentFilename = editor.document.fileName;
+        let filenameBase = currentFilename.slice(0, -path.extname(currentFilename).length + 1);
+
+        let column = 1;
+
+        async function openFile(extension: string) {
+            let filename = filenameBase + extension;
+            if (fs.existsSync(filename)) {
+                let doc = await vscode.workspace.openTextDocument(filename);
+                vscode.window.showTextDocument(doc, column, true);
+                column++;
+            }
+        }
+
+        await openFile("ts");
+        await openFile("html");
+        await openFile("scss");
+
     });
 
     context.subscriptions.push(disposable);
